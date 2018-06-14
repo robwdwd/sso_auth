75709512e520980c6b7793b0fa5ae915b66cb41d
Rob Woodward <robwdwd@icloud.com>
2018-06-14 16:57:00 +0100

    Costmetic changes

--- lib/Auth.php
+++ lib/Auth.php
@@ -63,18 +63,13 @@ class Auth
     public function logout()
     {
 
-      // ------------------------------------------------------------------------------------------------------------------------
-      // logout():
-      //		Log the user out and clear the session
-      // ------------------------------------------------------------------------------------------------------------------------
-
         session_name($this->session_name);
 
-      // Unset all of the session variables.
+        // Unset all of the session variables.
         $_SESSION = array();
 
-      // If it's desired to kill the session, also delete the session cookie.
-      // Note: This will destroy the session, and not just the session data!
+        // If it's desired to kill the session, also delete the session cookie.
+        // Note: This will destroy the session, and not just the session data!
         if (ini_get("session.use_cookies")) {
             $params = session_get_cookie_params();
             setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
--- templates/login.tpl
+++ templates/login.tpl
@@ -1,11 +1,10 @@
 {% include 'header.tpl' %}
 
-
 <form action="{{urlpath}}login" method="post" class="pure-form pure-form-aligned">
     <fieldset>
         <legend>Login</legend>
         <input type="hidden" name="{{form_token_key}}" value="{{form_token}}" />
-	    <input type="hidden" name="target" value="{{x_target}}" />
+        <input type="hidden" name="target" value="{{x_target}}" />
         <div class="pure-control-group">
             <label for="username">Username</label>
             <input id="username" name="username" type="text" placeholder="Username">
@@ -21,7 +20,7 @@
             <label for="errors"></label>
             <span class="pure-form-message-inline">
                 {% for error in errors %}
-                {{error|raw}}<br />
+                {{error}}<br />
                 {% endfor %}
             </span>
         </div>
